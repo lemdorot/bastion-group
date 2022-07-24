d31a74a9e7d5a9c927d3ee729adeb41f6eed4b19
lemdorot <lemdorot@gmail.com>
2022-07-24 22:16:30 +0300

    Добавил в список с продуктами тестовые данные

--- src/store/reducers/productReducer.ts
+++ src/store/reducers/productReducer.ts
@@ -1,6 +1,14 @@
 import { ProductState, ProductAction, ProductActionTypes } from './../../types/product';
 const initialState: ProductState = {
-    products: [],
+    products: [{ id: 111, name: 'Опора тавровая хомутовая ТХ', productTypeId: 0, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 112, name: 'Опора корпусная приварная КП', productTypeId: 1, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 113, name: 'Опора корпусная хомутовая КХ', productTypeId: 0, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 114, name: 'Опора подвижная ОПБ1', productTypeId: 1, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 115, name: 'Опора подвижная ОПБ2', productTypeId: 0, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 116, name: 'Опора трубчатая ТР', productTypeId: 1, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 117, name: 'Опора тавровая приварная ТП', productTypeId: 1, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 118, name: 'Опора подвижная ОПП3', productTypeId: 1, price: '849,9', gost: 'ГОСТ 14911-82'  },
+    { id: 119, name: 'Опора подвижная ОПП2', productTypeId: 0, price: '849,9', gost: 'ГОСТ 14911-82'  },],
     success: null,
     error: null
 }
